a5a560bbf6c2853e6d2b5bbd1b4f18a829d615b7
Ravit Sharma <sharma.ravit@gmail.com>
2021-06-24 15:41:31 -0700

    Adding implementation for pnet model.

--- 3PXNet_models/face_detection_custom/main/facenet_full_prec.cpp
+++ 3PXNet_models/face_detection_custom/main/facenet_full_prec.cpp
@@ -15,12 +15,32 @@
  * @return          Scores for every pixel, and box offset with respect.
  */
 mtmn_net_t *pnet_lite_f(dl_matrix3du_t *in) {
-	//Test: Running input through a single convolutional layer with dummy weights
-	//In this test, nullptr is returned, so the output is not connected to the output of the conv operator
-	//Weights taken from mnist example, defined in include/facenet_full_prec.h
+	dl_matrix3d_t *out_conv_1 = dl_matrix3duf_conv_common(in, &pnet_conv2d_kernel1, &pnet_conv2d_bias1, 1, 1, PADDING_VALID);
+    dl_matrix3d_relu(out_conv_1);
+    dl_matrix3d_t *out_pool_1 = dl_matrix3d_pooling(out_conv_1, 2, 2, 2, 2, PADDING_SAME, DL_POOLING_MAX);
 
-	dl_matrix3d_t *o1_1 = dl_matrix3dff_conv_common(in, &conv2d_kernel, &conv2d_bias, 1, 1, PADDING_SAME);
-	return nullptr;
+    dl_matrix3d_t *out_conv_2 = dl_matrix3dff_conv_common(out_pool_1, &pnet_conv2d_kernel2, &pnet_conv2d_bias2, 1, 1, PADDING_VALID);
+    dl_matrix3d_relu(out_conv_2);
+
+    dl_matrix3d_t *out_conv_3 = dl_matrix3dff_conv_common(out_conv_2, &pnet_conv2d_kernel3, &pnet_conv2d_bias3, 1, 1, PADDING_VALID);
+    dl_matrix3d_relu(out_conv_3);
+
+    dl_matrix3d_t *category = dl_matrix3dff_conv_common(out_conv_3, &pnet_conv2d_kernel4, &pnet_conv2d_bias4, 1, 1, PADDING_VALID);
+    dl_matrix3d_softmax(category); //TODO: How to indicate that this should be done over axis 3?
+    dl_matrix3d_t *offset = dl_matrix3dff_conv_common(out_conv_3, &pnet_conv2d_kernel5, &pnet_conv2d_bias5, 1, 1, PADDING_VALID);
+
+    //TODO: Call free as soon as tensors are no longer live
+    dl_matrix3d_free(out_conv_1);
+    dl_matrix3d_free(out_pool_1);
+    dl_matrix3d_free(out_conv_2);
+    dl_matrix3d_free(out_conv_3);
+
+    mtmn_net_t *result = (mtmn_net_t*) dl_lib_calloc(1, sizeof *result, 0);
+    result->category = category;
+    result->offset = offset;
+    result->landmark = nullptr;
+
+	return result;
 }
 
 /**
