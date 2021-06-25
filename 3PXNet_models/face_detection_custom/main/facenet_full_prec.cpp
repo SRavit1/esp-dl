5f735362dbccc53c805eab092338e1b9826478da
Ravit Sharma <sharma.ravit@gmail.com>
2021-06-25 07:42:39 -0700

    Add score verification.

--- 3PXNet_models/face_detection_custom/main/facenet_full_prec.cpp
+++ 3PXNet_models/face_detection_custom/main/facenet_full_prec.cpp
@@ -86,6 +86,11 @@ mtmn_net_t *rnet_lite_f_with_score_verify(dl_matrix3du_t *in, float threshold) {
     dl_matrix3d_free(out_conv_3);
     dl_matrix3d_free(out_dense_1);
 
+    if (category->item[0] < threshold) {
+        dl_matrix3d_free(category);
+        dl_matrix3d_free(offset);
+        return nullptr;
+    }
 
     mtmn_net_t *result = (mtmn_net_t*) dl_lib_calloc(1, sizeof *result, 0);
     result->category = category;
@@ -103,50 +108,6 @@ mtmn_net_t *rnet_lite_f_with_score_verify(dl_matrix3du_t *in, float threshold) {
  * @return          Scores for every box, box offset, and landmark with respect.
  */
 mtmn_net_t *onet_lite_f_with_score_verify(dl_matrix3du_t *in, float threshold) {
-/*
-# From MTCNN: https://github.com/ipazc/mtcnn/blob/master/mtcnn/network/factory.py
-
-conv2D_layer1 = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding="valid")
-conv2D_layer2 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding="valid")
-conv2D_layer3 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding="valid")
-conv2D_layer4 = Conv2D(128, kernel_size=(2, 2), strides=(1, 1), padding="valid")
-prelu_layer1 = ReLU() #PReLU(shared_axes=[1, 2])
-prelu_layer2 = ReLU() #PReLU(shared_axes=[1, 2])
-prelu_layer3 = ReLU() #PReLU(shared_axes=[1, 2])
-prelu_layer4 = ReLU() #PReLU(shared_axes=[1, 2])
-prelu_layer5 = ReLU() #PReLU()
-dense_layer1 = Dense(256)
-dense_layer2 = Dense(2)
-dense_layer3 = Dense(4)
-dense_layer4 = Dense(10)
-
-o_inp = Input((48, 48, 3))
-o_layer = conv2D_layer1(o_inp)
-o_layer = prelu_layer1(o_layer)
-o_layer = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same")(o_layer)
-
-o_layer = conv2D_layer2(o_layer)
-o_layer = prelu_layer2(o_layer)
-o_layer = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="valid")(o_layer)
-
-o_layer = conv2D_layer3(o_layer)
-o_layer = prelu_layer3(o_layer)
-o_layer = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="same")(o_layer)
-
-o_layer = conv2D_layer4(o_layer)
-o_layer = prelu_layer4(o_layer)
-
-o_layer = Flatten()(o_layer)
-o_layer = dense_layer1(o_layer)
-o_layer = prelu_layer5(o_layer)
-
-o_layer_out1 = dense_layer2(o_layer)
-o_layer_out1 = Softmax(axis=1)(o_layer_out1)
-o_layer_out2 = dense_layer3(o_layer)
-o_layer_out3 = dense_layer4(o_layer)
-
-o_net = Model(o_inp, [o_layer_out2, o_layer_out3, o_layer_out1])
-*/
     dl_matrix3d_t *out_conv_1 = dl_matrix3duf_conv_common(in, &onet_conv2d_kernel1, &onet_conv2d_bias1, 1, 1, PADDING_VALID);
     dl_matrix3d_relu(out_conv_1);
     dl_matrix3d_t *out_pool_1 = dl_matrix3d_pooling(out_conv_1, 3, 3, 2, 2, PADDING_SAME, DL_POOLING_MAX);
@@ -189,6 +150,12 @@ o_net = Model(o_inp, [o_layer_out2, o_layer_out3, o_layer_out1])
     dl_matrix3d_free(out_conv_3);
     dl_matrix3d_free(out_dense_1);
 
+    if (category->item[0] < threshold) {
+        dl_matrix3d_free(category);
+        dl_matrix3d_free(offset);
+        dl_matrix3d_free(landmark);
+        return nullptr;
+    }
 
     mtmn_net_t *result = (mtmn_net_t*) dl_lib_calloc(1, sizeof *result, 0);
     result->category = category;
