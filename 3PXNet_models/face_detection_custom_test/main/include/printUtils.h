da94d3ea1aeeb29510c18aca5d3d04fca579feb9
Ravit Sharma <sharma.ravit@gmail.com>
2021-07-02 19:46:04 -0700

    Adding function to print quantized matrix.

--- 3PXNet_models/face_detection_custom_test/main/include/printUtils.h
+++ 3PXNet_models/face_detection_custom_test/main/include/printUtils.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #include "dl_lib_matrix3d.h"
+#include "dl_lib_matrix3dq.h"
 #include "mtmn.h"
 
 void printMatrix(dl_matrix3d_t* mat) {
@@ -11,11 +12,17 @@ void printMatrix(dl_matrix3d_t* mat) {
     printf("Value is [ ");
     if (mat->item) {
         for (int i = 0; i < mat->h * mat->w * mat->c; i++) {
-            printf("%f ", mat->item[i]);
+            printf("%f ,", mat->item[i]);
             if (i%8 == 7) printf("\n");
         }
     }
     printf("]\n");
+}
+
+void printMatrixQu(dl_matrix3dq_t* mat) {
+    dl_matrix3d_t* mat_f = dl_matrix3d_from_matrixq(mat);
+    printMatrix(mat_f);
+    dl_matrix3d_free(mat_f);
 }
 
 void printOutput(mtmn_net_t* out) {
