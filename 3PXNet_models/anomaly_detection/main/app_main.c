56643a33cb59cc1015fa3d15b598ed59bf5ea620
Ravit Sharma <sharma.ravit@gmail.com>
2021-09-10 10:48:59 -0700

    anomaly detection project update.

---
+++ 3PXNet_models/anomaly_detection/main/app_main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "binarize/source.h"
+
+void test(void *arg)
+{
+    while(1)
+    {
+        main();
+
+        vTaskDelay(100);
+    }
+}
+
+void app_main()
+{
+    xTaskCreatePinnedToCore(&test, "test", 4096, NULL, 5, NULL, 0);
+}
