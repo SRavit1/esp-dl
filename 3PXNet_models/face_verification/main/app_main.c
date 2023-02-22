581d469e4ada3b2899eb32cf1507d545b541fe70
Ravit Sharma <sharma.ravit@gmail.com>
2023-02-22 09:11:26 -0800

    Minor changes.

--- 3PXNet_models/face_verification/main/app_main.c
+++ 3PXNet_models/face_verification/main/app_main.c
@@ -41,8 +41,6 @@ static void task(void *pvParameters) {
 
 void app_main()
 {
-    const void *flash_mmap_ptr;
-
     ESP_ERROR_CHECK(nvs_flash_init() );
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
